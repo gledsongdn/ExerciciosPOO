61a5bf4478e44704a2273d8acf58d3b71a8ee3d1
Gledson Santos <Gledsonvicente@gmail.com>
2019-02-26 22:28:27 -0300

    update

---
+++ src/aula01/BalancoTrimestral.java
@@ -0,0 +1,19 @@
+// Gledson Vicente Rodrigues
+// RA: N336870
+
+package aula01;
+
+public class BalancoTrimestral {
+
+    public static void main(String[] args) {
+
+        int gastosJaneiro = 1500;
+        int gastosFevereiro = 2300;
+        int gastosMarco = 1700;
+
+        int gastosTrimestrais = gastosJaneiro + gastosFevereiro + gastosMarco;
+
+        System.out.println(gastosTrimestrais);
+
+    }
+}
