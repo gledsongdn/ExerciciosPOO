3f3476b10ae2f91e493ea304464e457d6e42ebef
Gledson Santos <Gledsonvicente@gmail.com>
2019-03-04 11:09:44 -0300

    update

--- src/aula03/Tarefa3.java
+++ src/aula03/Tarefa3.java
@@ -2,9 +2,34 @@
 
 package aula03;
 
+import javax.swing.*;
+
 public class Tarefa3 {
 
     public static void main(String[] args) {
 
+        int a, b, c;
+
+        a = Integer.parseInt(JOptionPane.showInputDialog(null, "Primeiro número:"));
+        b = Integer.parseInt(JOptionPane.showInputDialog(null, "Segundo número:"));
+        c = Integer.parseInt(JOptionPane.showInputDialog(null, "Terceiro número:"));
+
+        if (a > b) {
+            if (b > c) {
+                JOptionPane.showMessageDialog(null, "Ordem decrescente\n" + a + "\n" + b + "\n" + c);
+            } else if (a > c) {
+                JOptionPane.showMessageDialog(null, "Ordem decrescente\n" + a + "\n" + c + "\n" + b);
+            } else {
+                JOptionPane.showMessageDialog(null, "Ordem decrescente\n" + c + "\n" + a + "\n" + b);
+            }
+        } else if (b > c) {
+            if (a > c) {
+                JOptionPane.showMessageDialog(null, "Ordem decrescente\n" + b + "\n" + a + "\n" + c);
+            } else {
+                JOptionPane.showMessageDialog(null, "Ordem decrescente\n" + b + "\n" + c + "\n" + a);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Ordem decrescente\n" + c + "\n" + b + "\n" + a);
+        }
     }
 }
--- src/aula03/Tarefa5.java
+++ src/aula03/Tarefa5.java
@@ -2,9 +2,30 @@
 
 package aula03;
 
+import javax.swing.*;
+
 public class Tarefa5 {
 
     public static void main(String[] args) {
 
+        int a, b, c;
+
+        a = Integer.parseInt(JOptionPane.showInputDialog(null, "Primeiro número:"));
+        b = Integer.parseInt(JOptionPane.showInputDialog(null, "Segundo número:"));
+        c = Integer.parseInt(JOptionPane.showInputDialog(null, "Terceiro número:"));
+
+        if ((a > b) && (a > c) && (b > c)) {
+            JOptionPane.showMessageDialog(null, "Maior: " + a + "\nMenor: " + c);
+        } else if ((a > b) && (a > b) && (c > b)) {
+            JOptionPane.showMessageDialog(null, "Maior: " + a + "\nMenor: " + b);
+        } else if ((b > a) && (b > c) && (a > c)) {
+            JOptionPane.showMessageDialog(null, "Maior: " + b + "\nMenor: " + c);
+        } else if ((b > a) && (b > c) && (c > a)) {
+            JOptionPane.showMessageDialog(null, "Maior: " + b + "\nMenor: " + a);
+        } else if ((c > a) && (c > b) && (a > b)) {
+            JOptionPane.showMessageDialog(null, "Maior: " + c + "\nMenor: " + b);
+        } else {
+            JOptionPane.showMessageDialog(null, "Maior: " + c + "\nMenor: " + a);
+        }
     }
 }
