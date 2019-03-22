84ac53dec569be4fcfb6ed9e5f41362dcbcf7eaa
gledson <gledsonvicente@gmail.com>
2019-03-22 19:41:18 -0300

    update aula 04

--- src/aula04/sistema/bancario/Conta.java
+++ src/aula04/sistema/bancario/Conta.java
@@ -7,17 +7,25 @@ public class Conta {
     double saldo;
     double limite;
     double salario;
-    double valorSaque;
 
     //Métodos
     /*Métdo de saque*/
-    void saque(double valor){
+    void saque(double valor) {
         double novoSaldo = this.saldo - valor;
         this.saldo = novoSaldo;
     }
 
-    void deposita(double valor){
-        double novoSaldo = this.saldo + valor;
-        this.saldo = novoSaldo;
+    void deposita(double valor) {
+        this.saldo += valor;
+    }
+
+    /*public double lucro() {
+        this.saldo += (this.saldo * 0.10);
+        return this.saldo;
+    }*/
+    void rendimento(double valorRendimento) {
+        double novosaldo = valorRendimento * this.saldo / 100 + this.saldo;
+//        double novosaldo = valorRendimento * 10 / 100;
+        this.saldo = novosaldo;
     }
 }
--- src/aula04/sistema/bancario/SistemaBancario.java
+++ src/aula04/sistema/bancario/SistemaBancario.java
@@ -10,15 +10,22 @@ public class SistemaBancario {
         cliente.nome = JOptionPane.showInputDialog(null, "Cliente: ");
 
         Conta conta = new Conta();
-        conta.saldo = Double.parseDouble(JOptionPane.showInputDialog(null,"Digite o saldo: "));
+        conta.saldo = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o saldo: "));
 
         //imprimir as informações
-        JOptionPane.showMessageDialog(null, cliente.nome +"\n"+conta.saldo);
+        JOptionPane.showMessageDialog(null, cliente.nome + "\n" + conta.saldo);
 
         conta.saque(Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o valor de saque: ")));
-        JOptionPane.showMessageDialog(null, "Saldo após saque: " +conta.saldo);
+        JOptionPane.showMessageDialog(null, "Saldo após saque: " + conta.saldo);
 
         conta.deposita(Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o valor do depósito: ")));
-        JOptionPane.showMessageDialog(null, "Saldo após depósito: " +conta.saldo);
+        JOptionPane.showMessageDialog(null, "Saldo após depósito: " + conta.saldo);
+
+//        Criar metodo para adicionar o valor de 10% no saldo
+//        JOptionPane.showMessageDialog(null, "Lucro: " + conta.lucro());
+        conta.rendimento(Double.parseDouble(JOptionPane.showInputDialog(null, "Valor do rendimento: ")));
+        JOptionPane.showMessageDialog(null, "Saldo após lucro: " + conta.saldo);
+
+
     }
 }
